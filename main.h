dcfbf8d9dc84933b0bab325c38f919d5487539a4
Thermiprettie <tadekolasarah@yahoo.com>
2022-10-16 09:24:46 -0700

    main file

--- main.h
+++ main.h
@@ -40,5 +40,21 @@ char *convert(unsigned long int num, int base, int lowercase);
 int _printf(const char *format, ...);
 
 int _putchar(char c);
+int _puts(char *str);
+
+int (*get_print(char s))(va_list, flags_t *);
+
+int get_flag(char s, flags_t *f);
+
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+int print_address(va_list l, flags_t *f);
+
+int print_percent(va_list l, flags_t *f);
 
 #endif
