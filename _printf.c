a7af023d1a64874b2485fe00039760f6eea81244
Thermiprettie <tadekolasarah@yahoo.com>
2022-10-16 08:48:10 -0700

    correction

--- _printf.c
+++ _printf.c
@@ -9,18 +9,15 @@
 
 int _printf(const char *format, ...)
 {
-	int ch, i;
+	int ch;
 	va_list args;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
-
-	for (i = 0; i < args; i++)
-	{
-		ch += printf(format, args[i], args[i]);
-	}
+	
+	ch = printf(format, args, args);
 
 	return (ch);
 }
