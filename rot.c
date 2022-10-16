ae92dbcdc3aff28b1be828ae1968f00d965f0511
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-10-16 17:32:09 -0400

    0x11

--- rot.c
+++ rot.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
 *print_R - Print a string in rot13
 *@va: va_list arguments
@@ -8,31 +9,69 @@
 
 int print_R(va_list va)
 {
-	int i, j, counter = 0;
-	int k = 0;
-	char *s = va_arg(args, char*);
-	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
+	char *s;
+	int i;
+	char n[] = "(null)";
+
+	s = rot13(malloc_str(va_arg(va, char *)));
 	if (s == NULL)
-		s = "(null)";
-	for (i = 0; s[i]; i++)
 	{
-		k = 0;
-		for (j = 0; alpha[j] && !k; j++)
+		for (i = 0; n[i] != '\0'; i++)
+			_putchar(n[i]);
+		return (6);
+	}
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	free(s);
+	return (i);
+}
+
+/**
+*malloc_str - create a new space in memory to copy the string
+*@str: string
+*Return: string
+*/
+
+char *malloc_str(char *str)
+{
+	char *copy;
+	unsigned int j, i;
+
+	if (str ==  NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	copy = malloc((i + 1) * sizeof(char));
+	if (copy == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+		copy[j] = str[j];
+	return (copy);
+}
+
+/**
+ *rot13 - String to rot13
+ *@p: pointer the string we want to convert in rot13
+ *Return: string
+ */
+char *rot13(char *p)
+{
+	int j;
+	int i;
+	char a1[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+	char a2[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+
+	for (j = 0; p[j] != '\0'; j++)
+	{
+		for (i = 0; a1[i] != '\0'; i++)
 		{
-			if (s[i] == alpha[j])
+			if (p[j] == a1[i])
 			{
-				_putchar(beta[j]);
-				counter++;
-				k = 1;
+				p[j] = a2[i];
+				break;
 			}
 		}
-		if (!k)
-		{
-			_putchar(s[i]);
-			counter++;
-		}
 	}
-	return (counter);
+	return (p);
 }
